8a9f718953ef3b1ec0e02b233ee54c58f6372b6e
christianf21 <christianfeob@yahoo.com>
2018-09-28 16:58:24 -0400

    initial commit and added migrations for film and genre

---
+++ database/migrations/2018_09_28_204727_create_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('name')->unique();
+			$table->string('description');
+			$table->date('release_date');
+			$table->decimal('rating', 5)->default(1);
+			$table->string('country');
+			$table->integer('genre_id');
+			$table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
